23c5aa17f95c4e9d066bd51ebaa2faaad650e2b1
Sooraj Bhaskaran <sooraj.bhaskaran@bigbinary.com>
2022-09-22 01:43:05 +0530

    Improvements in new contact UI

--- app/javascript/src/components/Dashboard/Contacts/utils.jsx
+++ app/javascript/src/components/Dashboard/Contacts/utils.jsx
@@ -3,30 +3,37 @@ import React from "react";
 import { MenuVertical } from "neetoicons";
 import { Avatar, Typography, Dropdown } from "neetoui";
 
-const renderAvatarWithNameRoleAndImage = (name, { role, imageUrl }) => (
+const renderAvatarWithNameRoleAndImage = (
+  name,
+  { firstName, lastName, role, imageUrl }
+) => (
   <div className="flex flex-row items-center">
     <Avatar
       className="mr-2"
       user={{
-        name,
+        name: `${firstName} ${lastName}`,
         imageUrl,
       }}
     />
     <div>
-      <Typography style="h4">{name}</Typography>
+      <Typography style="h4">{`${firstName} ${lastName}`}</Typography>
       <Typography style="body2">{role}</Typography>
     </div>
   </div>
 );
 
-const renderDropdownButton = (setShowDeleteAlert, setSelectedContactId, id) => (
+const renderDropdownButton = ({
+  changeDeleteAlert,
+  changeSelectedId,
+  contactId,
+}) => (
   <div>
     <Dropdown buttonStyle="text" icon={MenuVertical}>
       <li> Edit </li>
       <li
         onClick={() => {
-          setShowDeleteAlert(true);
-          setSelectedContactId(id);
+          changeDeleteAlert(true);
+          changeSelectedId(contactId);
         }}
       >
         Delete
@@ -66,6 +73,10 @@ export const buildContactColumnData = (
     title: "",
 
     render: (item, { id }) =>
-      renderDropdownButton(setShowDeleteAlert, setSelectedContactId, id),
+      renderDropdownButton({
+        changeDeleteAlert: setShowDeleteAlert,
+        changeSelectedId: setSelectedContactId,
+        contactId: id,
+      }),
   },
 ];
