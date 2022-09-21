5067f3cc6f1bb07197a61bfd9c2be5075cd43f8e
Sooraj Bhaskaran <sooraj.bhaskaran@bigbinary.com>
2022-09-21 13:00:34 +0530

    Added contact list page UI

---
+++ app/javascript/src/components/Dashboard/Contacts/constants.js
@@ -0,0 +1,56 @@
+export const CONTACTS = [
+  {
+    id: 1,
+    name: "John Doe",
+    email: "johndoe@gmail.com",
+    role: "Owner",
+    imageUrl:
+      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    createdAt: new Date("February 5, 2021"),
+  },
+  {
+    id: 2,
+    name: "Joe Doe",
+    email: "joedoe@gmail.com",
+    role: "Owner",
+    imageUrl:
+      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    createdAt: new Date("February 5, 2021"),
+  },
+  {
+    id: 3,
+    name: "Jane Doe",
+    email: "janedoe@gmail.com",
+    role: "Owner",
+    imageUrl:
+      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    createdAt: new Date("February 5, 2021"),
+  },
+  {
+    id: 4,
+    name: "John Doe",
+    email: "johndoe@gmail.com",
+    role: "Owner",
+    imageUrl:
+      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    createdAt: new Date("February 5, 2021"),
+  },
+  {
+    id: 5,
+    name: "Joe Doe",
+    email: "joedoe@gmail.com",
+    role: "Owner",
+    imageUrl:
+      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    createdAt: new Date("February 5, 2021"),
+  },
+  {
+    id: 6,
+    name: "Jane Doe",
+    email: "janedoe@gmail.com",
+    role: "Owner",
+    imageUrl:
+      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    createdAt: new Date("February 5, 2021"),
+  },
+];
